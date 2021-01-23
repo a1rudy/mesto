a279387afce7c2a3d7c0fb4c20e0f50775869a67
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-01-23 18:59:16 +0300

    feat: Project 4: ver 4.2.0 (fixed)

---
+++ script/index.js
@@ -0,0 +1,40 @@
+let formElement = document.querySelector('.popup');
+
+let nameInput = formElement.querySelector('.popup__form_type_name');
+let jobInput = formElement.querySelector('.popup__form_type_job');
+
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = formElement.querySelector('.popup__close-button');
+let saveButton = formElement.querySelector('.popup__save-button');
+
+let addName = document.querySelector('.profile__name');
+let addJob = document.querySelector('.profile__description');
+
+function openPopup() {
+  formElement.classList.add('popup_opened');
+
+  nameInput.value = addName.textContent;
+  jobInput.value = addJob.textContent;
+}
+
+function closePopup() {
+  nameInput.value = ''
+  jobInput.value = ''
+  
+  formElement.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  addName.textContent = nameInput.value;
+  addJob.textContent = jobInput.value;
+
+  formElement.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('click', formSubmitHandler);
+
+formElement.addEventListener('submit', formSubmitHandler); 

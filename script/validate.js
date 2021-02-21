689bb8b077195abf3a11c90fa42616a7dc4429b5
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-02-22 00:16:13 +0300

    feat: Добавление объекта функции и закрытие по overlay

--- script/validate.js
+++ script/validate.js
@@ -1,41 +1,41 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, inputErrorClass, errorClass) => {
   // console.log(inputElement.name, errorMessage);
   const errorELement = formElement.querySelector(`.${inputElement.id}-error`);
 
-  inputElement.classList.add('popup__input_type_error');
-  errorELement.classList.add('popup__input-error_active');
+  inputElement.classList.add(inputErrorClass);
+  errorELement.classList.add(errorClass);
   errorELement.textContent = errorMessage;
 };
 // 5.1 функция на добавление текста ошибки про невалидном инпуте
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, inputErrorClass, errorClass) => {
   const errorELement = formElement.querySelector(`.${inputElement.id}-error`);
 
-  inputElement.classList.remove('popup__input_type_error');
-  errorELement.classList.remove('popup__input-error_active');
+  inputElement.classList.remove(inputErrorClass);
+  errorELement.classList.remove(errorClass);
   errorELement.textContent = '';
 };
 // 6.1 функция на удаление текста ошибки про валидном инпуте
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
   const isInputElementValid = inputElement.validity.valid
 
   if (!isInputElementValid) {
     const errorMessage = inputElement.validationMessage;
-    showInputError(formElement, inputElement, errorMessage);
+    showInputError(formElement, inputElement, errorMessage, inputErrorClass, errorClass);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, inputErrorClass, errorClass);
   }
 };
 // 4.1 проверяем валидность введенного инпута и передаем функцию для добавления или удаления текста ошибки
 
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.setAttribute('disabled', true);
-    buttonElement.classList.add('popup__save-button_inactive');
+    buttonElement.classList.add(inactiveButtonClass);
   } else {
     buttonElement.removeAttribute('disabled');
-    buttonElement.classList.remove('popup__save-button_inactive');
+    buttonElement.classList.remove(inactiveButtonClass);
   }
 };
 // 5.2 добавляем отключение кнопки сабмита, если все инпуты невалидны, убираем, если валидны;
@@ -47,7 +47,7 @@ const hasInvalidInput = (inputList) => {
 };
 // 4.2 проверяем, все ли инпуты НЕ валидны, если да, возвращает true, если нет - false;
 
-const setEventListeners = (formElement, inputSelector, submitButtonSelector) => {
+const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) => {
   formElement.addEventListener('submit', (event) => {
     event.preventDefault();
   });
@@ -58,12 +58,12 @@ const setEventListeners = (formElement, inputSelector, submitButtonSelector) => 
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass);
     });
   });
 
-  toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass);
 };
 // 2. отменяем стандартное событие сабмита;
 // 3.1 находим все инпуты на странице и передаем им функцию на проверку валидности;
@@ -82,7 +82,7 @@ enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
-  inactiveButtonClass: '.popup__save-button_inactive',
-  inputErrorClass: '.popup__input_type_error',
-  errorClass: '.popup__input-error_active',
+  inactiveButtonClass: 'popup__save-button_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active',
 });
